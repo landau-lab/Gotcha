14af3c18520ab225104a58a48625a24d6e0169af
Franco Izzo <fizzo13@users.noreply.github.com>
2022-06-22 11:29:38 -0400

    Add files via upload

--- R/BatchMutationCalling.R
+++ R/BatchMutationCalling.R
@@ -1,3 +1,28 @@
+#' Define read genotype and read counts per genotype for each cell barcode using parallel job submission to slurm cluster
+#'
+#' @param out Path to the fastq or filtered fastq files
+#' @param whitelist.file.path Path to the file containing the whitelist with possible cell barcodes
+#' @param wt.max.mismatch Integer indicating the number of accepted missmatches when performing pattern matching for the wild-type sequence
+#' @param mut.max.mismatch Integer indicating the number of accepted missmatches when performing pattern matching for the mutant sequence
+#' @param keep.raw.reads Logical. Whether to return the raw reads in the output file. Defaults to false
+#' @param ncores Integer indicating the number of cores to use for parallel processing
+#' @param reverse.complement Whether to take the reverse complement of the cell barcodes
+#' @param testing Logical indicating whether to sample the first 1,000 reads for testing the function
+#' @param which.read Which read to select to look for the mutation site
+#' @param atac.barcodes Logical indicating whether to only use the detected atac barcodes in the experiment as whitelist
+#' @param primer.sequence Character vector of length one indicating the primer sequence
+#' @param primed.max.mismatch  Integer indicating the maximum number of mismatches accepted when searching for the primer sequence
+#' @param atac.barcodes.file.path Path to the file containing the cell barcodes detected in the experiment
+#' @param wt.sequence Character vector of length one specifying the expected wild-type sequence
+#' @param mut.sequence Character vector of length one specifying the expected mutant sequence
+#' @param mutation.start Position in which the expected wild-type or mutant sequence starts in the read
+#' @param mutation.end Position in which the expected wild-type or mutant sequence ends in the read
+#' @param soptions List class object specifying the parameters for the jobs submitted to the slurm cluster
+#' @return Archr Project with added genotyping columns into the metadata
+#' @examples
+#'
+
+
 BatchMutationCalling = function(out = "/path_to_filtered_fastqs/",
                                 whitelist.file.path = "/path_to_whitelist/whitelist.txt",
                                 wt.max.mismatch = 0,
@@ -17,6 +42,8 @@ BatchMutationCalling = function(out = "/path_to_filtered_fastqs/",
                                 mutation.end = 34,
                                 soptions = list(mem = '20g', 'cpus-per-task' = 10)
 ){
+
+  options(expressions = 2.5e5) # Increase the number of nested expressions to be evaluated. Limit is 5e5.
 
   WhiteListMatch <- WTcount <- MUTcount <- WT <- MUT <- NULL # To prevent non-declared global variables
 
