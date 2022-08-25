2a84feb043820b4e7989df0450caeb1dcef0c3de
theob0t <theo06bot75@gmail.com>
2022-08-25 15:57:07 -0400

    Remove GotchaLabeling wrapper function - waiting for approval

--- R/zzz.R
+++ R/zzz.R
@@ -1,50 +1,37 @@
 # Function to create and load the virtual environment to use the .py gotcha script
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage(
-    "
-
-▒█▀▀█ █▀▀█ ▀▀█▀▀ ░░ ▒█▀▀█ █░░█ ░█▀▀█
-▒█░▄▄ █░░█ ░░█░░ ▀▀ ▒█░░░ █▀▀█ ▒█▄▄█
-▒█▄▄█ ▀▀▀▀ ░░▀░░ ░░ ▒█▄▄█ ▀░░▀ ▒█░▒█
-
-"
-  )
-  
+.onAttach <- function(libname, pkgname){
   packageStartupMessage("Checking if r-reticulate-gotcha virtual environment is available...")
-  if (!reticulate::virtualenv_exists(envname = "r-reticulate-gotcha")) {
+  if(!reticulate::virtualenv_exists(envname = "r-reticulate-gotcha")){
     packageStartupMessage("r-reticulate-gotcha virtual environment not found, setting up environment...")
-    
+
     # Create isolated virtual environment using reticulate to install python modules
-    reticulate::virtualenv_create(
-      envname = "r-reticulate-gotcha",
-      python = Sys.which("python3"),
-      version = "python3",
-      packages = c("pip", "wheel"),
-      pip_version = "pip-22.1.1"
-    )
-    
+    reticulate::virtualenv_create(envname = "r-reticulate-gotcha",
+                                  #python = Sys.which("python3"),
+								  python = "C:/Users/theo0/AppData/Local/Programs/Python/Python310/python.exe" ,
+								  #python = "C:/Users/theo0/OneDrive/Documents/.virtualenvs/r-reticulate-gotcha/Scripts/python.exe",
+                                  version = "python3",
+                                  packages = c("pip","wheel"),
+                                  pip_version = "pip-22.1.1"
+								  )
+
     # Set up reticulate to use the virtual environment
     reticulate::use_virtualenv("r-reticulate-gotcha")
-    
-    # Install required python modules
-    reticulate::py_install(
-      packages = c("pandas", "matplotlib", "seaborn", "numpy", "sklearn"),
-      python_version = "python3",
-      pip = T,
-      envname = "r-reticulate-gotcha"
-    )
-    
-    
-  } else{
+
+
+	# Install required python modules
+    reticulate::py_install(packages = c("pandas","matplotlib","seaborn","numpy","sklearn"),
+                           python_version = "python3",
+                           pip = T,
+                           envname = "r-reticulate-gotcha")
+
+
+
+  }else{
     # Use the r-reticulate-gotcha virtual environment
     reticulate::use_virtualenv("r-reticulate-gotcha")
-    packageStartupMessage(
-      "Virutal environment r-reticulate-gotcha is available | use_virtualenv(r-reticulate-gotcha)"
-    )
+    packageStartupMessage("Virutal environment r-reticulate-gotcha is available | use_virtualenv(r-reticulate-gotcha)")
+
   }
 }
 
-download.file(
-  'https://raw.github.com/landau-lab/Gotcha/development/inst/python/gotcha_labeling.py',
-  paste0(find.package('Gotcha'), '/gotcha_labeling.py')
-)
+#source_python('test.py')
