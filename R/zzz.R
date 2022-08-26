5d0062b9bdf9be2f124108909150c02f61100563
theob0t <theo06bot75@gmail.com>
2022-08-26 17:14:21 -0400

    Revert to 866198b

--- R/zzz.R
+++ R/zzz.R
@@ -6,32 +6,30 @@
 
     # Create isolated virtual environment using reticulate to install python modules
     reticulate::virtualenv_create(envname = "r-reticulate-gotcha",
-                                  #python = Sys.which("python3"),
-								  python = "C:/Users/theo0/AppData/Local/Programs/Python/Python310/python.exe" ,
-								  #python = "C:/Users/theo0/OneDrive/Documents/.virtualenvs/r-reticulate-gotcha/Scripts/python.exe",
+                                  python = Sys.which("python3"),
                                   version = "python3",
                                   packages = c("pip","wheel"),
-                                  pip_version = "pip-22.1.1"
-								  )
+                                  pip_version = "pip-22.1.1")
 
     # Set up reticulate to use the virtual environment
     reticulate::use_virtualenv("r-reticulate-gotcha")
 
-
-	# Install required python modules
+    # Install required python modules
     reticulate::py_install(packages = c("pandas","matplotlib","seaborn","numpy","sklearn"),
                            python_version = "python3",
                            pip = T,
                            envname = "r-reticulate-gotcha")
 
 
-
   }else{
+
     # Use the r-reticulate-gotcha virtual environment
     reticulate::use_virtualenv("r-reticulate-gotcha")
     packageStartupMessage("Virutal environment r-reticulate-gotcha is available | use_virtualenv(r-reticulate-gotcha)")
 
   }
-}
 
-#source_python('test.py')
+  # Source labeling functions from GitHub
+  packageStartupMessage("Sourcing python script from GitHub...")
+  reticulate::source_python(file = "https://github.com/landau-lab/Gotcha/blob/main/gotcha_labeling.py?raw=TRUE", envir = globalenv())
+}
