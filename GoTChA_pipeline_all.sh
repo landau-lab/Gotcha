87b6b2f0acc24a9721a8567e039e501bf47b4d48
nwh84 <noellew8@gmail.com>
2025-03-12 18:30:26 -0400

    remove submit files

--- GoTChA_pipeline_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bsub -J split < GoTChA_pipeline_submit_split.sh
-
-bsub -w 'done(split)' -J filt < GoTChA_pipeline_submit_filt.sh
-
-bsub -w 'done(filt)' -J call_mut < GoTChA_pipeline_submit.sh
-
